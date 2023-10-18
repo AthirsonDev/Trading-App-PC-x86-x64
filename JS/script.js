e61c9f9f1fba51447786519d864beb3c06d7adf5
Athirson <athirsonantunes@gmail.com>
2023-10-18 10:41:34 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('.changeColor')
+const textColor = document.querySelector('.color')
+function randomColorrgb(){
+    let r = Math.floor(Math.random()* 256)
+    let g = Math.floor(Math.random()* 256)
+    let b = Math.floor(Math.random()* 256)
+    let color = `rgb(${r},${g},${b})`
+    return color
+}
+function randomColorhex(){
+    let letters = '0123456789ABCDEF'; // caracteres possíveis para uma cor hexadecimal
+    let color = '#'; // inicia a string da cor com '#'
+    for (let i = 0; i < 6 ; i++){
+      color += letters[Math.floor(Math.random()*16)]; // adiciona um caractere aleatório à string da cor
+    }
+    return color; // retorna a cor gerada
+  }
+button.addEventListener('click', ()=>{
+  let color = randomColorrgb();
+  document.documentElement.style.setProperty('--bg-color', color);
+  textColor.textContent = color
+})
